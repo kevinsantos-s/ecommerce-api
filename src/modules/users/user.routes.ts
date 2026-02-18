1e568c81f8132c2649860a3ed56e1bdb16c0e37c
Kevin Santos Da Silva <kevinssilva1502@gmail.com>
2026-02-18 19:22:00 -0300

    feat: rotas de usuário prontas

--- src/modules/users/user.routes.ts
+++ src/modules/users/user.routes.ts
@@ -5,4 +5,8 @@ export const userRoutes = Router();
 
 const controller = new UserController();
 
-userRoutes.post("/create", (req, res) => controller.create(req, res));
+userRoutes.post("/", (req, res) => controller.create(req, res));
+userRoutes.get("/:id", (req, res) => controller.findById(req, res));
+userRoutes.get("/", ( req, res ) => controller.findAll( req, res ));
+userRoutes.put("/:id", ( req, res ) => controller.update( req, res ));
+userRoutes.delete("/:id", ( req, res) => controller.delete( req, res));

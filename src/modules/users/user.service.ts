cbb9ce13f761a2fc0b5b4ef53face54ca61350a3
Kevin Santos Da Silva <kevinssilva1502@gmail.com>
2026-02-17 21:49:54 -0300

    feat: criação de service com método para criação de usuário com validações de campo vazio simples e @ no email e transformação de senha em hash

--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -1,11 +1,39 @@
-import { prisma } from "../../client"
-import { createUser, userResponse } from "./user.interface";
+import { prisma } from "../../client";
+import { CreateUserDTO, UserResponseDTO } from "./user.interface";
+import bcrypt from "bcrypt";
+import { AppError } from "../../utils/errors";
 
-export class UserService{
-    async create(data: createUser): Promise<userResponse>{
-        const user = await prisma.user.create({
-            data,
-        })
-        return user;
+export class UserService {
+  async create(data: CreateUserDTO): Promise<UserResponseDTO> {
+    const { email, password, name } = data;
+
+    if (!email || !password || !name) {
+      throw new AppError("Insira todos os campos!", 400);
     }
+
+    const alreadyExists = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (alreadyExists) {
+      throw new AppError("Já existe um usuário com este Email!", 409);
+    }
+
+    if (!email.includes("@")) {
+      throw new AppError("Email inválido!", 400);
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await prisma.user.create({
+      data: { email: email, password: hashedPassword, name: name },
+    });
+
+    return {
+      id: newUser.id,
+      name: newUser.name,
+      email: newUser.email,
+      createdAt: newUser.createdAt,
+    };
+  }
 }

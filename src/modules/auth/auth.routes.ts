c4e4bb67fabb15d70afca70792086548eea71fce
Kevin Santos Da Silva <kevinssilva1502@gmail.com>
2026-02-21 14:58:43 -0300

    refactor: alteração de rotas para usar Zod para validar

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { AuthController } from "./auth.controller";
+import { validateData } from "../../middleware/validate.middleware";
+import { loginSchema } from "./auth.schema";
 
 export const authRoutes = Router();
 
 const controller = new AuthController();
 
-authRoutes.post("/login", (req, res) => controller.auth(req, res));
+authRoutes.post("/login", validateData({ body: loginSchema }), controller.auth.bind(controller));

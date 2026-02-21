1caac17bd933cec9123b7bb020a70710f85edacd
Kevin Santos Da Silva <kevinssilva1502@gmail.com>
2026-02-21 14:56:43 -0300

    refactor: atualizando rotas para usar Zod para validar antes as requisições

--- src/modules/users/user.routes.ts
+++ src/modules/users/user.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { UserController } from "./user.controller";
+import { validateData } from "../../middleware/validate.middleware";
+import { createUserSchema, idParamSchema, updateUserSchema } from "./user.schema";
 
 export const userRoutes = Router();
 
 const controller = new UserController();
 
-userRoutes.post("/", (req, res) => controller.create(req, res));
-userRoutes.get("/:id", (req, res) => controller.findById(req, res));
-userRoutes.get("/", ( req, res ) => controller.findAll( req, res ));
-userRoutes.put("/:id", ( req, res ) => controller.update( req, res ));
-userRoutes.delete("/:id", ( req, res) => controller.delete( req, res));
+userRoutes.post("/", validateData({ body: createUserSchema }), controller.create.bind(controller));
+userRoutes.get("/:id", validateData({ params: idParamSchema }), controller.findById.bind(controller));
+userRoutes.get("/", controller.findAll.bind(controller));
+userRoutes.put("/:id", validateData({ params: idParamSchema, body: updateUserSchema }), controller.update.bind(controller));
+userRoutes.delete("/:id", validateData({ params: idParamSchema }), controller.delete.bind(controller));

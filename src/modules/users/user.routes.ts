89daf688142f5d14338277c8c9527f92967efbab
Kevin Santos <kevinssilva1502@gmail.com>
2026-02-25 16:18:12 -0300

    feat: middleware de role verificando role nas rotas

--- src/modules/users/user.routes.ts
+++ src/modules/users/user.routes.ts
@@ -3,13 +3,14 @@ import { UserController } from "./user.controller";
 import { validateData } from "../../middleware/validate.middleware";
 import { createUserSchema, idParamSchema, updateUserSchema } from "./user.schema";
 import { authenticateToken } from "../auth/auth.middleware";
+import { authorizeRole } from "../../middleware/role.middleware";
 
 export const userRoutes = Router();
 
 const controller = new UserController();
 
 userRoutes.post("/", validateData({ body: createUserSchema }), (req, res) => controller.create(req, res));
-userRoutes.get("/:id", authenticateToken, validateData({ params: idParamSchema }), (req, res) => controller.findById(req, res));
-userRoutes.get("/", authenticateToken, (req, res) => controller.findAll(req, res));
-userRoutes.put("/:id", authenticateToken, validateData({ params: idParamSchema, body: updateUserSchema }), (req, res) => controller.update(req, res));
-userRoutes.delete("/:id", authenticateToken, validateData({ params: idParamSchema }), (req, res) => controller.delete(req, res));
+userRoutes.get("/:id", authenticateToken,  authorizeRole("ADMIN"), validateData({ params: idParamSchema }), (req, res) => controller.findById(req, res));
+userRoutes.get("/", authenticateToken, authorizeRole("ADMIN"), (req, res) => controller.findAll(req, res));
+userRoutes.put("/:id", authenticateToken, authorizeRole("USER"), validateData({ params: idParamSchema, body: updateUserSchema }), (req, res) => controller.update(req, res));
+userRoutes.delete("/:id", authenticateToken,  authorizeRole("USER"), validateData({ params: idParamSchema }), (req, res) => controller.delete(req, res));

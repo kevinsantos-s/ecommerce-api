4a70155393b810134e73ddba7b5562a5ae378e3b
Kevin Santos <kevinssilva1502@gmail.com>
2026-02-26 14:05:53 -0300

    feat: rota de logout criada

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -34,14 +34,26 @@ export class AuthController {
     try {
       const accessToken = await this.service.refresh(req.cookies.refreshToken);
 
-           res.cookie("accessToken", accessToken, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
-            maxAge: 15 * 60 * 1000
-        })
+      res.cookie("accessToken", accessToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: 15 * 60 * 1000,
+      });
 
       return ResponseHandler.sucess(res, null);
+    } catch (error) {
+      return errorHandler(res, error);
+    }
+  }
+
+  async logout(req: Request, res: Response) {
+    try {
+      res.clearCookie("refreshToken", {
+        path: "/auth/refresh",
+      });
+      res.clearCookie("accessToken");
+      return ResponseHandler.sucess(res, null, "Deslogado com sucesso");
     } catch (error) {
       return errorHandler(res, error);
     }

d9236a504ee8fd7362958602e354e139a6f784bf
Kevin Santos <kevinssilva1502@gmail.com>
2026-02-26 14:06:19 -0300

    feat: rota de logout nas routes criado

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { AuthController } from "./auth.controller";
 import { validateData } from "../../middleware/validate.middleware";
 import { loginSchema } from "./auth.schema";
+import { authenticateToken } from "./auth.middleware";
 
 export const authRoutes = Router();
 
@@ -9,3 +10,5 @@ const controller = new AuthController();
 
 authRoutes.post("/login", validateData({ body: loginSchema }), (req, res) => controller.auth(req, res));
 authRoutes.post("/refresh", (req, res) => controller.refresh(req, res));
+authRoutes.post("/logout", authenticateToken, (req, res) => controller.logout(req, res));
+

e4d4080d4ff344433bcd9e48ea081c5a7f998f41
Kevin Santos Da Silva <kevinssilva1502@gmail.com>
2026-02-25 21:47:43 -0300

    feat: middleware de autorização por role adicionado

--- src/middleware/role.middleware.ts
+++ src/middleware/role.middleware.ts
@@ -1,11 +1,11 @@
-import { ResponseHandler } from "../utils/responseHandler"
-import { Request, Response, NextFunction} from "express"
+import { ResponseHandler } from "../utils/responseHandler";
+import { Request, Response, NextFunction } from "express";
 
-export function authorizeRole(role: string){
-    return (req: Request, res: Response, next: NextFunction) => {
-        if(req.user?.role !== role){
-            return ResponseHandler.forbidden(res)
-        }
-        next()
+export function authorizeRole(...roles: string[]) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (!req.user || !roles.includes(req.user.role)) {
+      return ResponseHandler.forbidden(res);
     }
+    next();
+  };
 }

5c7bdf94489a292abbe4f12b5c8339f24d48a5a2
Kevin Santos <kevinssilva1502@gmail.com>
2026-02-26 14:06:49 -0300

    refactor: utilizando roles centralizado ao invés de passar as entidades direto

--- src/modules/users/user.routes.ts
+++ src/modules/users/user.routes.ts
@@ -4,13 +4,14 @@ import { validateData } from "../../middleware/validate.middleware";
 import { createUserSchema, idParamSchema, updateUserSchema } from "./user.schema";
 import { authenticateToken } from "../auth/auth.middleware";
 import { authorizeRole } from "../../middleware/role.middleware";
+import { ROLES } from "../../config/roles";
 
 export const userRoutes = Router();
 
 const controller = new UserController();
 
 userRoutes.post("/", validateData({ body: createUserSchema }), (req, res) => controller.create(req, res));
-userRoutes.get("/:id", authenticateToken,  authorizeRole("USER", "SELLER", "ADMIN"), validateData({ params: idParamSchema }), (req, res) => controller.findById(req, res));
-userRoutes.get("/", authenticateToken, authorizeRole("ADMIN"), (req, res) => controller.findAll(req, res));
-userRoutes.put("/:id", authenticateToken, authorizeRole("USER", "SELLER", "ADMIN"), validateData({ params: idParamSchema, body: updateUserSchema }), (req, res) => controller.update(req, res));
-userRoutes.delete("/:id", authenticateToken,  authorizeRole("USER", "SELLER", "ADMIN"), validateData({ params: idParamSchema }), (req, res) => controller.delete(req, res));
+userRoutes.get("/:id", authenticateToken,  authorizeRole(...ROLES.ALL), validateData({ params: idParamSchema }), (req, res) => controller.findById(req, res));
+userRoutes.get("/", authenticateToken, authorizeRole(...ROLES.ADMIN_ONLY), (req, res) => controller.findAll(req, res));
+userRoutes.put("/:id", authenticateToken, authorizeRole(...ROLES.ALL), validateData({ params: idParamSchema, body: updateUserSchema }), (req, res) => controller.update(req, res));
+userRoutes.delete("/:id", authenticateToken,  authorizeRole(...ROLES.ALL), validateData({ params: idParamSchema }), (req, res) => controller.delete(req, res));

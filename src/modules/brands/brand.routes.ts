df07a481d594406b65d065285fd4c97735b2c7c1
Kevin Santos <kevinssilva1502@gmail.com>
2026-03-05 14:00:54 -0300

    feat: adicionando middleware de seller para verificar se o seller está como aprovado pelo admin

--- src/modules/brands/brand.routes.ts
+++ src/modules/brands/brand.routes.ts
@@ -6,13 +6,14 @@ import { validateData } from "../../middleware/validate.middleware";
 import { authenticateToken } from "../auth/auth.middleware";
 import { authorizeRole } from "../../middleware/role.middleware"; 
 import { idParamSchema } from "../../utils/schemas";
+import { checkSellerApproved } from "../../middleware/seller.middleware";
 
 export const brandRoutes = Router();
 
 const controller = new BrandController()
 
-brandRoutes.post("/", authenticateToken, validateData({ body: createBrandSchema }), authorizeRole(...ROLES.SELLER_ONLY), (req, res) => controller.create(req, res));
+brandRoutes.post("/", authenticateToken, authorizeRole(...ROLES.SELLER_ONLY), checkSellerApproved, validateData({ body: createBrandSchema }), (req, res) => controller.create(req, res));
 brandRoutes.get("/:id", validateData({ params: idParamSchema }), (req, res) => controller.findById(req, res));
 brandRoutes.get("/", (req, res) => controller.findAll(req, res));
-brandRoutes.put("/:id", authenticateToken, authorizeRole(...ROLES.SELLER_ONLY), validateData({ params: idParamSchema, body: updateBrandSchema }), (req, res) => controller.update(req, res));
-brandRoutes.delete("/:id", authenticateToken,  authorizeRole(...ROLES.SELLER_ONLY), validateData({ params: idParamSchema }), (req, res) => controller.delete(req, res));
+brandRoutes.put("/:id", authenticateToken, authorizeRole(...ROLES.SELLER_ONLY), checkSellerApproved, validateData({ params: idParamSchema, body: updateBrandSchema }), (req, res) => controller.update(req, res));
+brandRoutes.delete("/:id", authenticateToken,  authorizeRole(...ROLES.SELLER_ONLY), checkSellerApproved, validateData({ params: idParamSchema }), (req, res) => controller.delete(req, res));
